68b477fd2ee3b59290f62c02f3fda8f98382eff2
Doug Daniels <ddaniels@mortardata.com>
2013-02-01 17:52:48 -0500

    Remove the word tamp, which appears inside several other common words

--- udfs/python/coffee.py
+++ udfs/python/coffee.py
@@ -2,7 +2,7 @@ from pig_util import outputSchema
 
 COFFEE_SNOB_PHRASES = set((\
     'espresso', 'cappucino', 'macchiato', 'latte', 'cortado', 'pour over', 'barista', 
-    'flat white', 'siphon pot', 'woodneck', 'french press', 'arabica', 'tamp', 'chemex', 
+    'flat white', 'siphon pot', 'woodneck', 'french press', 'arabica', 'chemex', 
     'frothed', 'la marzocco', 'mazzer', 'la pavoni', 'nespresso', 'rancilio silvia', 'hario',
     'intelligentsia', 'counter culture', 'barismo', 'sightglass', 'blue bottle', 'stumptown',
     'single origin', 'coffee beans', 'coffee grinder', 'lavazza', 'coffeegeek'\
